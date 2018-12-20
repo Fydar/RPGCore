ff5cc2da42df944e2363f911fbd70d76a16e026b
Anthony Marmont <dev.anthonymarmont@gmail.com>
2018-12-20 23:22:28 +0000

    Updates to the Cursor System custom editor to allow for a dragable pointer.

--- RPGCore/Assets/RPGCore/Scripts/UI/Cursor/CursorStyle.cs
+++ RPGCore/Assets/RPGCore/Scripts/UI/Cursor/CursorStyle.cs
@@ -24,14 +24,50 @@ namespace RPGCore.UI.CursorManagement
 
 			public override void OnInspectorGUI ()
 			{
-				DrawDefaultInspector ();
-
-				Rect rect = GUILayoutUtility.GetRect (0, 64);
-				GUI.Box (rect, GUIContent.none, EditorStyles.helpBox);
-
 				CursorStyle style = (CursorStyle)target;
+				DrawDefaultInspector ();
+				
+				GUILayout.Space (20);
 
-				if (rect.Contains (Event.current.mousePosition))
+				Rect pointRow = GUILayoutUtility.GetRect (0, 96);
+				Rect pointRect = new Rect (pointRow)
+				{
+					x = pointRow.x + (pointRow.width * 0.5f) - (pointRow.height * 0.5f),
+					width = pointRow.height,
+				};
+				GUI.Box (pointRect, GUIContent.none, EditorStyles.helpBox);
+
+				GUI.DrawTexture (pointRect, style.Graphic);
+				if (Event.current.type == EventType.MouseDown || Event.current.type == EventType.MouseDrag)
+				{
+					if (pointRect.Contains(Event.current.mousePosition))
+					{
+						style.Hotspot = new Vector2 (
+							Mathf.InverseLerp (pointRect.xMin, pointRect.xMax, Event.current.mousePosition.x) * style.Graphic.width,
+							Mathf.InverseLerp (pointRect.yMin, pointRect.yMax, Event.current.mousePosition.y) * style.Graphic.height
+						);
+					}
+				}
+
+				Vector2 pointerPoint = new Vector2 (
+					Mathf.Lerp (pointRect.xMin, pointRect.xMax, style.Hotspot.x / style.Graphic.width),
+					Mathf.Lerp (pointRect.yMin, pointRect.yMax, style.Hotspot.y / style.Graphic.height));
+
+				float pointerSize = 4;
+				Rect pointer = new Rect (pointerPoint.x - pointerSize, pointerPoint.y - pointerSize, pointerSize * 2, pointerSize * 2);
+				GUI.Box (pointer, "");
+				
+				GUILayout.Space (20);
+
+				Rect rect = GUILayoutUtility.GetRect (0, 96);
+				Rect previewRect = new Rect (rect)
+				{
+					x = rect.x + (rect.width * 0.5f) - (rect.height * 0.5f),
+					width = rect.height,
+				};
+				GUI.Box (previewRect, GUIContent.none, EditorStyles.helpBox);
+
+				if (previewRect.Contains (Event.current.mousePosition))
 				{
 					if (CursorManager.CurrentStyle != style)
 						CursorManager.SetCursor (style);
@@ -42,7 +78,7 @@ namespace RPGCore.UI.CursorManagement
 						CursorManager.SetCursor (CursorManager.GetStyle ("Default"));
 				}
 
-				EditorGUIUtility.AddCursorRect (rect, MouseCursor.CustomCursor);
+				EditorGUIUtility.AddCursorRect (previewRect, MouseCursor.CustomCursor);
 			}
 
 			public override Texture2D RenderStaticPreview (string assetPath, Object[] subAssets, int width, int height)
