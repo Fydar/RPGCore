e03a9ab0210f4f4e13a3a1d13dd1b5c63c1a1ad6
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-01-04 22:31:15 +0000

    Added a placeholder ChargesInputNode for items with charges.

---
+++ RPGCore/Assets/RPGCore/Scripts/Item/Nodes/ChargesInputNode.cs
@@ -0,0 +1,28 @@
+﻿using RPGCore.Behaviour;
+using RPGCore.Behaviour.Connections;
+
+namespace RPGCore
+{
+	[NodeInformation ("Item/Charges Input", "Input", OnlyOne = true)]
+	public class ChargesInputNode : BehaviourNode, IInputNode<ItemSurrogate>
+	{
+		public IntOutput Charges;
+		public EventInput ConsumeCharge;
+		public EventOutput OnConsumeCharge;
+
+		protected override void OnSetup (IBehaviourContext context)
+		{
+
+		}
+
+		protected override void OnRemove (IBehaviourContext context)
+		{
+
+		}
+
+		public void SetTarget (IBehaviourContext context, ItemSurrogate target)
+		{
+
+		}
+	}
+}
