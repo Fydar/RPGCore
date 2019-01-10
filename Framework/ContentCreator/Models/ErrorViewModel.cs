77c6d79a76b57ec8076a0238217486ae31cdac0e
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-01-10 15:46:46 +0000

    Added the ContentCreator, a project that utilises Electron.Net to replace the BehaviourGraph window written inside Unity.

---
+++ Framework/ContentCreator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContentCreator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
