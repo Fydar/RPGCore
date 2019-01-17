5d30db3058fb83dc98c36aeb81a97ade4048fa0c
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-01-17 16:48:36 +0000

    Removed large quantities of duplicate code in the BehaviourGraph nodes. Reusing code for StatCollectionInput nodes for Armour and Weapons. This could be extended to add different types of values of a specific type, such as Flasks and their capacity.

--- RPGCore/Assets/RPGCore/Scripts/Item/Nodes/ArmourReadNode.cs
+++ RPGCore/Assets/RPGCore/Scripts/Item/Nodes/ArmourReadNode.cs
@@ -1,68 +1,14 @@
 ﻿using RPGCore.Behaviour;
 using RPGCore.Behaviour.Connections;
+using RPGCore.Stats;
 using System;
 
 namespace RPGCore
 {
-	[NodeInformation ("Item/Read Armour Stat", "Attribute")]
-	public class ArmourReadNode : BehaviourNode
+    [NodeInformation ("Item/Read Armour Stat", "Attribute")]
+	public class ArmourReadNode : StatCollectionReadNode<ArmourStatFloatInputCollection,
+		ArmourStatInstanceCollection, ArmourStatEntry, ArmourInputNode>
 	{
-		public WeaponStatEntry Stat;
 
-		public ItemInput Target;
-		public FloatOutput Value;
-
-		protected override void OnSetup (IBehaviourContext context)
-		{
-			ConnectionEntry<ItemSurrogate> targetInput = Target[context];
-			ConnectionEntry<float> valueInput = Value[context];
-
-			Action updateListener = () =>
-			{
-				var weaponNode = targetInput.Value.Template.GetNode<WeaponInputNode> ();
-
-				if (weaponNode == null)
-				{
-					valueInput.Value = 0;
-					return;
-				}
-
-				var localStatInput = weaponNode.GetStat (targetInput.Value, Stat);
-
-				valueInput.Value = localStatInput.Value;
-			};
-
-			if (targetInput.Value != null)
-			{
-				targetInput.Value.Template.GetNode<ArmourInputNode> ()
-					.Armour[targetInput.Value].OnAfterChanged += updateListener;
-
-				updateListener ();
-			}
-
-			targetInput.OnBeforeChanged += () =>
-			{
-				if (targetInput.Value == null)
-					return;
-
-				targetInput.Value.Template.GetNode<ArmourInputNode> ()
-					.Armour[targetInput.Value].OnAfterChanged -= updateListener;
-			};
-
-			targetInput.OnAfterChanged += () =>
-			{
-				if (targetInput.Value == null)
-					return;
-
-				targetInput.Value.Template.GetNode<ArmourInputNode> ()
-					.Armour[targetInput.Value].OnAfterChanged += updateListener;
-				updateListener ();
-			};
-		}
-
-		protected override void OnRemove (IBehaviourContext context)
-		{
-
-		}
 	}
 }
---
+++ RPGCore/Assets/RPGCore/Scripts/Item/Nodes/StatCollectionInputNode.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPGCore.Behaviour;
+using RPGCore.Behaviour.Connections;
+using RPGCore.Stats;
+
+namespace RPGCore
+{
+	public abstract class StatCollectionInputNode<A, B> : BehaviourNode
+		where A : EnumerableCollection<FloatInput>
+		where B : EnumerableCollection<StatInstance>, new()
+	{
+		public WeaponStatFloatInputCollection Inputs;
+
+		public CharacterOutput HitTarget;
+		public EventOutput OnHit;
+
+		[NonSerialized]
+		public Dictionary<IBehaviourContext, B> StatsMapping = new Dictionary<IBehaviourContext, B> ();
+
+		public B GetStats (IBehaviourContext context)
+		{
+			return StatsMapping[context];
+		}
+
+		public StatInstance GetStat (ItemSurrogate context, CollectionEntry entry)
+		{
+			B statsCollection = StatsMapping[context];
+			StatInstance stat = statsCollection[entry];
+			return stat;
+		}
+
+		protected override void OnSetup (IBehaviourContext context)
+		{
+			var statsCollection = new B ();
+			StatsMapping[context] = statsCollection;
+			statsCollection.GetEnumerator ();
+
+			var inputsEnumerator = Inputs.GetEnumerator();
+			foreach(var stat in GetStats(context))
+			{
+				inputsEnumerator.MoveNext();
+				var currentInput = inputsEnumerator.Current[context];
+
+				var modifier = stat.AddFlatModifier(currentInput.Value);
+				currentInput.OnAfterChanged += () => modifier.Value = currentInput.Value;
+			}}
+
+		protected override void OnRemove (IBehaviourContext context)
+		{
+
+		}
+	}
+}
