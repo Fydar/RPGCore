8892e4b86da6aba62f6712f5f7b135f5c3a198e7
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-01-21 07:55:03 +0000

    Ordered some folders and added an interface for node instance serialization.

---
+++ RPGCore/Assets/RPGCore/Scripts/Behaviour/INodeSerialization.cs
@@ -0,0 +1,8 @@
+﻿namespace RPGCore.Behaviour
+{
+	public interface INodeSerialization
+	{
+		string Serialize (IBehaviourContext context);
+		void Deserialize (IBehaviourContext context, string data);
+	}
+}
