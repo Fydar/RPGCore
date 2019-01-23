93a64745b648406802ed1d6d9fa684fc3a569ad9
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-01-23 13:35:12 +0000

    Removed the last remaining demo views and added file listening to the open menu. Currently solving an issue where the process hangs performing "Electron.IpcMain.Send", this could be due to JavaScript garbage collection.

--- Framework/ContentCreator/wwwroot/assets/imports.js
+++ Framework/ContentCreator/wwwroot/assets/imports.js
@@ -1,12 +1,8 @@
-const links = document.querySelectorAll('link[rel="import"]')
+const links = document.querySelectorAll('link[rel="import"]');
 
 // Import and add each page to the DOM
 Array.prototype.forEach.call(links, function (link) {
-  let template = link.import.querySelector('.task-template')
-  let clone = document.importNode(template.content, true)
-  if (link.href.match('about.html')) {
-    document.querySelector('body').appendChild(clone)
-  } else {
-    document.querySelector('.content').appendChild(clone)
-  }
+	let template = link.import.querySelector('.task-template');
+	let clone = document.importNode(template.content, true);
+	document.querySelector('.content').appendChild(clone);
 })
