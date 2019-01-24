951119b6dfebf4f4a905cba8abd1bb5479cc7bb0
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-01-24 22:08:59 +0000

    Framework: Seperated the gathering of InputSockets and OutputSockets into 2 passes.

--- Framework/Behaviour/Nodes/Node.cs
+++ Framework/Behaviour/Nodes/Node.cs
@@ -4,22 +4,30 @@
 	{
 		public abstract INodeInstance Create();
 		public abstract void Setup(GraphInstance graph, INodeInstance metadata, Actor target);
-		public abstract SocketMap[] ConnectToken(GraphInstance graph, object instance);
+
+		public abstract InputMap[] Inputs(GraphInstance graph, object instance);
+		public abstract OutputMap[] Outputs(GraphInstance graph, object instance);
 	}
 
 	public abstract class Node<T> : Node
 		where T : INodeInstance, new()
 	{
-		public abstract SocketMap[] Connect(GraphInstance graph, T instance);
+		public abstract InputMap[] Inputs(GraphInstance graph, T instance);
+		public abstract OutputMap[] Outputs(GraphInstance graph, T instance);
 
 		public override INodeInstance Create()
 		{
 			return new T();
 		}
 
-		public sealed override SocketMap[] ConnectToken(GraphInstance graph, object instance)
+		public sealed override InputMap[] Inputs(GraphInstance graph, object instance)
 		{
-			return Connect(graph, (T)instance);
+			return Inputs(graph, (T)instance);
+		}
+
+		public sealed override OutputMap[] Outputs(GraphInstance graph, object instance)
+		{
+			return Outputs(graph, (T)instance);
 		}
 
 		public sealed override void Setup(GraphInstance graph, INodeInstance metadata, Actor target)
