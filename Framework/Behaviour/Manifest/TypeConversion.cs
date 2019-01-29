49a930a2e7cd490e9207eeba54b17f7d5dc1a7c7
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-01-29 16:26:19 +0000

    Framework: Added the TypeManifest, used for defining types and the conversions between them for node sockets and their fields.

---
+++ Framework/Behaviour/Manifest/TypeConversion.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace Behaviour.Manifest
+{
+	public struct TypeConversion
+	{
+		public string Name;
+		public string Description;
+		public string Type;
+
+		public static TypeConversion Construct(Type type, MethodInfo method)
+		{
+			var typeConversion = new TypeConversion
+			{
+				Name = type.Name + " -> " + method.ReturnType.Name,
+				Type = method.ReturnType.Name
+			};
+
+			return typeConversion;
+		}
+	}
+}
---
+++ Framework/Behaviour/Manifest/TypeInformation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Behaviour.Manifest
+{
+	public struct TypeInformation
+	{
+		public string Name;
+		public string Description;
+		public TypeConversion[] Conversions;
+
+		public static TypeInformation Construct(Type type)
+		{
+			var typeInformation = new TypeInformation
+			{
+				Name = type.Name
+			};
+			var conversions = new List<TypeConversion>();
+			
+			var methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static);
+
+			foreach(var method in methods)
+			{
+				if (method.Name == "op_Implicit")
+				{
+					conversions.Add(TypeConversion.Construct(method));
+				}
+				else if (method.Name == "op_Explicit")
+				{
+					conversions.Add(TypeConversion.Construct(method));
+				}
+			}
+
+			typeInformation.Conversions = conversions.ToArray();
+			return typeInformation;
+		}
+	}
+}
