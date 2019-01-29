a0d9ef0ee1c1712c5422ec8115f971d88585ee31
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-01-29 16:12:22 +0000

    Framework: Made a graphs name editable by the ContentCreator and laid down the groundwork where the JavaScript node editor will function.

--- Framework/Runner/Program.cs
+++ Framework/Runner/Program.cs
@@ -8,15 +8,13 @@ namespace Runner
 
 		static void Main(string[] args)
 		{
-			Console.WriteLine ("-----------------------");
-
 			Console.ForegroundColor = ConsoleColor.Gray;
+			Console.WriteLine ("-----------------------");
 			Console.Write (" RPGCore ");
 			Console.ForegroundColor = ConsoleColor.Yellow;
 			Console.Write ("2.0");
 			Console.ForegroundColor = ConsoleColor.Gray;
 			Console.Write (" Framework\n");
-
 			Console.WriteLine ("-----------------------");
 
 			simulator = new Simulator();
