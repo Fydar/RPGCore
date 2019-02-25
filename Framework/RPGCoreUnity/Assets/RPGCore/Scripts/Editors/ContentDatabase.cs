e06b179e2677f1daf4271d93fde8247ab9f5eab3
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-02-25 17:04:38 +0000

    Framework: Updated Unity Scripting Runtime. Added ProejctConfiguration ScriptableObject.

---
+++ Framework/RPGCoreUnity/Assets/RPGCore/Scripts/Editors/ContentDatabase.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using RPGCore.Behaviour.Packages;
+
+public static class ContentDatabase
+{
+	public static PackageExplorer PrimaryPackage;
+}
