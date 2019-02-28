35662aafd21f9a534f79160fe56f4f81fc099bd3
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-02-28 14:20:13 +0000

    Framework: Added a script that automatically copies RPGCore.Behaviour.dll to the Unity project.

---
+++ Framework/DistributeOutputs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+unity_project_path="$(git rev-parse --show-toplevel)/Framework/RPGCoreUnity"
+
+if [ ! -d "$unity_project_path" ]; then
+    echo "Couldn't find target destination to distribute build output."
+    exit 1
+fi
+cp -fv $1 $unity_project_path/$2
