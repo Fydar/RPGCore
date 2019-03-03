d5f6e377b230612df9686273309c79792b44ae39
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-03-03 18:02:59 +0000

    Framework: Created a window to browse the content of a package. Current it's only capable of listing all assets and viewing the Main.bhvr files.

---
+++ Framework/RPGCoreUnity/Assets/RPGCore/Scripts/Editors/BehaviourEditor.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEditor;
+using RPGCore.Packages;
+using System.Text;
+
+namespace RPGCore.Unity.Editors
+{
+    public class BehaviourEditor : EditorWindow
+    {
+        public PackageImport CurrentPackage;
+
+        public bool HasCurrentAsset;
+        public PackageAsset CurrentAsset;
+        public string CurrentAssetJson;
+
+        [MenuItem("Window/Behaviour")]
+        public static void Open()
+        {
+            var window = EditorWindow.GetWindow<BehaviourEditor>();
+
+            window.Show();
+        }
+
+        public void OnGUI()
+        {
+            CurrentPackage = (PackageImport)EditorGUILayout.ObjectField(CurrentPackage, typeof(PackageImport));
+
+            var explorer = CurrentPackage.Explorer;
+
+            foreach(var asset in explorer.Folders)
+            {
+                if (GUILayout.Button(asset.ToString()))
+                {
+                    CurrentAsset = asset;
+                    HasCurrentAsset = true;
+                    CurrentAssetJson = null;
+                }
+            }
+
+            if (HasCurrentAsset)
+            {
+                if (string.IsNullOrEmpty(CurrentAssetJson))
+                {
+                    var bhvr = FindBhvr(CurrentAsset);
+                    Debug.Log(bhvr);
+                    CurrentAssetJson = Encoding.UTF8.GetString(bhvr.LoadData());
+                }
+
+                CurrentAssetJson = EditorGUILayout.TextArea(CurrentAssetJson);
+            }
+        }
+
+	    public static PackageResource FindBhvr (PackageAsset asset)
+        {
+            for (int i = 0; i < asset.Assets.Length; i++)
+            {
+                if (asset.Assets[i].Name.EndsWith (".bhvr", System.StringComparison.Ordinal))
+                {
+                    return asset.Assets[i];
+                }
+            }
+            return default (PackageResource);
+        }
+    }
+}
