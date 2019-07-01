08b0ed34a840c4126c65c950eb014961b976b4cd
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-07-01 13:52:13 +0100

    🔥 Unify "Content" directories

--- Framework/RPGCore.Runner/Program.cs
+++ Framework/RPGCore.Runner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace RPGCore.Runner
 {
@@ -16,6 +17,12 @@ namespace RPGCore.Runner
 			Console.ForegroundColor = ConsoleColor.Gray;
 			Console.Write (" Framework\n");
 			Console.WriteLine ("-----------------------");
+
+			if (!File.Exists ("Content"))
+			{
+				var directory = new DirectoryInfo ("bin/Debug/netcoreapp2.2");
+				Directory.SetCurrentDirectory (directory.FullName);
+			}
 
 			simulator = new Simulator ();
 			simulator.Start ();
