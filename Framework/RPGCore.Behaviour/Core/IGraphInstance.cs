31520fa7f57a60033fd4d685e2125dcb7e850022
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-07-16 13:58:06 +0100

    🚧 Get ALL inputs from output socket

--- Framework/RPGCore.Behaviour/Core/IGraphInstance.cs
+++ Framework/RPGCore.Behaviour/Core/IGraphInstance.cs
@@ -1,11 +1,13 @@
-﻿namespace RPGCore.Behaviour
+﻿using System.Collections.Generic;
+
+namespace RPGCore.Behaviour
 {
 	public interface IGraphInstance : IBehaviour
 	{
 		INodeInstance this[LocalId id] { get; }
 		void Setup (Actor target);
 		InputSource GetSource<T> (Input<T> input);
-		OutputSource GetSource<T> (Output<T> output);
+		IEnumerable<OutputSource> GetSource<T> (Output<T> output);
 	}
 
 	public interface IGraphConnections
