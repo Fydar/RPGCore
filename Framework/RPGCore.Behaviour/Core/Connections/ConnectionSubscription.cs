785c2be614b77d20506f3d130c5b44b76032db4f
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-07-24 15:10:51 +0100

    🚧 Restructure connection subscriptions
    This optimizes connection unsubscribing.

---
+++ Framework/RPGCore.Behaviour/Core/Connections/ConnectionSubscription.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RPGCore.Behaviour
+{
+	public struct ConnectionSubscription
+	{
+		public INodeInstance Node;
+		public List<Action> Callbacks;
+
+		public ConnectionSubscription (INodeInstance node)
+		{
+			Node = node;
+			Callbacks = new List<Action> ();
+		}
+	}
+}
