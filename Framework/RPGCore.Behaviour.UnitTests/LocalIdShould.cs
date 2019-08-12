39327d02e4ba15c18980c7fbf7cd466878d31114
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-08-12 14:29:13 +0100

    🚧 Made LocalId prefix the string with "0x"
    This makes the outputted string apparent that it's is hexadecimal formatted.
    
    Added UnitTests for LocalIdShould to ensure that old ID formats are still valid and integer literals translate to the same LocalId. Added a final test to make sure that the "x" in the outputted string is case insensitive.

---
+++ Framework/RPGCore.Behaviour.UnitTests/LocalIdShould.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+
+namespace RPGCore.Behaviour.UnitTests
+{
+	public class LocalIdShould
+	{
+		[Test]
+		public void CaseInvarientParsing ()
+		{
+			int original = 1048471;
+
+			string upperHex = original.ToString ("X2");
+			string lowerHex = original.ToString ("x2");
+
+			var upperId = new LocalId (upperHex);
+			var lowerId = new LocalId (lowerHex);
+
+			Assert.AreEqual (upperId, lowerId);
+		}
+
+		[Test]
+		public void MatchesIntegerLiteral ()
+		{
+			var stringLiteral = new LocalId ("0xff001000");
+			var integerLiteral = new LocalId (0xff001000);
+
+			Assert.AreEqual (stringLiteral, integerLiteral);
+		}
+
+		[Test]
+		public void PrefixInvarientParsing ()
+		{
+			int original = 1048471;
+
+			string unprefixed = original.ToString ("x2");
+			string prefixed = "0x" + unprefixed;
+
+			var prefixedId = new LocalId (prefixed);
+			var unprefixedId = new LocalId (unprefixed);
+
+			Assert.AreEqual (prefixedId, unprefixedId);
+		}
+	}
+}
