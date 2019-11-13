a0491549180a83764448a858ced5775ba6b3c3ea
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-11-13 20:13:36 +0000

    🐛 More changes to make DistributeOutputs.sh soft-error

--- src/DistributeOutputs.sh
+++ src/DistributeOutputs.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 set -e
-unity_project_path="$(git rev-parse --show-toplevel)/src/RPGCoreUnity"
 
-if [ ! -d "$unity_project_path" ]; then
-    echo "Couldn't find target destination to distribute build output."
-    exit 0
-fi
+{
+    unity_project_path="$(git rev-parse --show-toplevel)/src/RPGCoreUnity"
 
-cp -fv $1 "$unity_project_path/$2"
+    if [ ! -d "$unity_project_path" ]; then
+        echo "Couldn't find target destination to distribute build output."
+        exit 0
+    fi
+    
+    cp -fv $1 "$unity_project_path/$2"
+} || {
+    echo "Unable to copy files to target destination to distribute build output."
+}
