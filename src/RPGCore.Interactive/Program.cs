133563839de3c50f185498c67dd338fb91ab3d9a
Anthony Marmont <dev.anthonymarmont@gmail.com>
2020-01-19 21:42:58 +0000

    🚧 Template project for RPGCore.Interactive

---
+++ src/RPGCore.Interactive/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace RPGCore.Interactive
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+		public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args)
+		{
+			return BlazorWebAssemblyHost.CreateDefaultBuilder ()
+				.UseBlazorStartup<Startup> ();
+		}
+	}
+}
