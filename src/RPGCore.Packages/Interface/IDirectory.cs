55a078710c2b10b24f24b16af876be35d745c1a3
Anthony Marmont <dev.anthonymarmont@gmail.com>
2020-02-26 22:44:09 +0000

    🚧 Added interfaces for directories referenced in #19

---
+++ src/RPGCore.Packages/Interface/IDirectory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RPGCore.Packages
+{
+	public interface IDirectory
+	{
+		string Name { get; }
+		string FullName { get; }
+
+		IReadOnlyList<IDirectory> Directories { get; }
+		IResourceCollection Resources { get; }
+	}
+}
---
+++ src/RPGCore.Packages/Project/ProjectDirectory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace RPGCore.Packages
+{
+	public class ProjectDirectory : IDirectory
+	{
+		public string Name => throw new System.NotImplementedException();
+
+		public string FullName => throw new System.NotImplementedException();
+
+		public IReadOnlyList<IDirectory> Directories => throw new System.NotImplementedException();
+
+		public IResourceCollection Resources => throw new System.NotImplementedException();
+	}
+}
