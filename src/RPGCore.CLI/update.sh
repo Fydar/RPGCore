49e92d56f4a34730c08c4b8df51afd496458395a
Anthony Marmont <dev.anthonymarmont@gmail.com>
2021-02-12 04:05:16 +0000

    🔥 Removed RPGCore.CLI

--- src/RPGCore.CLI/update.sh
+++
@@ -1,6 +0,0 @@
-dotnet pack --verbosity minimal
-
-dotnet tool uninstall --global RPGCore.CLI
-
-dotnet tool install --global --add-source ./nupkg RPGCore.CLI
-dotnet tool install --global dotnet-suggest
