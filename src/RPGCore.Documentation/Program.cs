8ee78aedc3dfd8646eebf9c3ab3a59951af395c4
Anthony Marmont <dev.anthonymarmont@gmail.com>
2021-02-23 03:36:23 +0000

    ✨ Added project containing code samples for documentation

---
+++ src/RPGCore.Documentation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace RPGCore.Documentation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			var directory = FindSourceDirectory();
+
+			Console.WriteLine(directory.FullName);
+		}
+
+
+		private static DirectoryInfo FindSourceDirectory()
+		{
+			var directory = new DirectoryInfo(Environment.CurrentDirectory);
+			while (directory != null)
+			{
+				if (directory.Name.Equals("RPGCore", StringComparison.OrdinalIgnoreCase))
+				{
+					return directory;
+				}
+
+				directory = directory.Parent;
+			}
+			return directory;
+		}
+	}
+}
---
+++ src/RPGCore.Documentation/Samples/AddNodeSample.cs
@@ -0,0 +1,58 @@
+﻿using RPGCore.Behaviour;
+
+namespace RPGCore.Documentation.Samples.AddNodeSample
+{
+	#region default
+	// Node Template Definition
+	public class AddNode : NodeTemplate<AddNode>
+	{
+		// Node Sockets
+		public InputSocket ValueA;
+		public InputSocket ValueB;
+
+		public OutputSocket Output;
+
+		// Boilerplate Create Method
+		public override Instance Create() => new AddInstance();
+
+		// Node Instance Definition
+		public class AddInstance : Instance
+		{
+			// Node Instance Connectors
+			public Input<float> ValueA;
+			public Input<float> ValueB;
+
+			public Output<float> Output;
+
+			// Socket to Connector Mapping
+			public override InputMap[] Inputs(ConnectionMapper connections) => new[]
+			{
+				connections.Connect(ref Template.ValueA, ref ValueA),
+				connections.Connect(ref Template.ValueB, ref ValueB)
+			};
+
+			public override OutputMap[] Outputs(ConnectionMapper connections) => new[]
+			{
+				connections.Connect(ref Template.Output, ref Output)
+			};
+
+			// Node Logic
+			public override void Setup()
+			{
+				// Subscribe to events, setup variables...
+			}
+
+			public override void OnInputChanged()
+			{
+				// Fires when an input value changes.
+				Output.Value = ValueA.Value + ValueB.Value;
+			}
+
+			public override void Remove()
+			{
+				// Unsubscribe from events, dispose members...
+			}
+		}
+	}
+	#endregion default
+}
