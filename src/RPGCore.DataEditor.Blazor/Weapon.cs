ac313a4017e6a168ec84883391573add86dbafc2
Anthony Marmont <dev.anthonymarmont@gmail.com>
2021-06-20 15:29:10 +0100

    🚧 Basic in-browser DataEditor implementation with Blazor

---
+++ src/RPGCore.DataEditor.Blazor/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using RPGCore.Data;
+using System;
+
+namespace RPGCore.DataEditor.Blazor
+{
+	[EditableType]
+	public class Weapon
+	{
+		public string Name { get; set; } = "Longsword";
+		public int Damage { get; set; } = 20;
+		public long Durability { get; set; } = 100;
+		public bool IsEnchantable { get; set; } = true;
+		public WeaponSlot[] Slots { get; set; } = Array.Empty<WeaponSlot>();
+	}
+
+	[EditableType]
+	public class WeaponSlot
+	{
+		public string SlotName { get; set; } = string.Empty;
+	}
+}
